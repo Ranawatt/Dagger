cfd6ce114088713c1e92fd2bb83257a5f278819a
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-06-19 23:50:05 +0530

    created ApplicationModule under module pkg to provide dependencies

---
+++ dagger-hilt/src/main/java/com/example/dagger_hilt/di/component/ApplicationComponent.kt
@@ -0,0 +1,7 @@
+package com.example.dagger_hilt.di.component
+
+import dagger.Component
+
+@Component
+class ApplicationComponent {
+}
--- dagger-hilt/src/main/java/com/example/dagger_hilt/di/module/ApplicationModule.kt
+++ dagger-hilt/src/main/java/com/example/dagger_hilt/di/module/ApplicationModule.kt
@@ -1,2 +1,54 @@
 package com.example.dagger_hilt.di.module
 
+
+import androidx.core.os.BuildCompat
+import com.example.dagger_hilt.BuildConfig
+import com.example.dagger_hilt.data.api.ApiHelper
+import com.example.dagger_hilt.data.api.ApiHelperImpl
+import com.example.dagger_hilt.data.api.ApiService
+import com.example.dagger_hilt.di.component.ApplicationComponent
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = BuildConfig.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    } else {
+        OkHttpClient.Builder().build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String) : Retrofit {
+        return Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiHelper(apiHelper: ApiHelperImpl) : ApiHelper = apiHelper
+}
